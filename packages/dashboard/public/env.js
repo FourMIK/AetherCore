1aaa738a9afa29f25af65e923eaa0a022b1d2202
jhook87 <justinhooker87@gmail.com>
2026-02-19 17:07:49 -0500

    Add files via upload

---
+++ packages/dashboard/public/env.js
@@ -0,0 +1,7 @@
+(function (window) {
+  window.__ENV__ = Object.freeze({
+    REACT_APP_API_URL: "http://localhost:8080/api",
+    REACT_APP_WS_URL: "ws://localhost:8080/ws",
+    REACT_APP_TPM_ENABLED: "false"
+  });
+})(window);
